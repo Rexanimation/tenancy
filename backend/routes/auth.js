61326a70f98099784889028cb9da224c64e9c9e5
Rexanimation <Rexanimation@users.noreply.github.com>
2025-12-23 01:00:53 -0800

    fix: explicit cookie path and auth debugging

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -35,6 +35,7 @@ router.get(
             secure: true,
             sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
+            path: '/'
         });
 
         res.redirect(`${process.env.FRONTEND_URL}/dashboard`);
